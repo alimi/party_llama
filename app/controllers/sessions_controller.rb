92f8d8979b5b8e160f41b3f27e2912d43d3707d1
Ali Ibrahim <aibrahim2k2@gmail.com>
2018-02-17 22:42:07 -0500

    Update SessionsController#create so it's a little more tiddy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    party = Party.find_by(reservation_code: params[:session][:reservation_code])
+    Current.party = Party.find_by(
+      reservation_code: params[:session][:reservation_code]
+    )
 
-    if party
-      session[:current_party_id] = party.id
-
-      if party.passed_submission_deadline?
-        redirect_to conclusion_path
-      else
-        redirect_to introduction_path
-      end
+    if Current.party
+      save_current_party_in_session
     else
       flash[:error] = "Sorry, we couldn't find your reservation. Please try again."
       redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def save_current_party_in_session
+    session[:current_party_id] = Current.party.id
+
+    if Current.party.passed_submission_deadline?
+      redirect_to conclusion_path
+    else
+      redirect_to introduction_path
     end
   end
 end
