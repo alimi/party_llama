0a8a24e61ef622a5813cfb184a6f4fe85b128f5f
Ali Ibrahim <aibrahim2k2@gmail.com>
2018-02-18 12:19:53 -0500

    Clear current party from session at conclusion

--- app/controllers/conclusions_controller.rb
+++ app/controllers/conclusions_controller.rb
@@ -2,5 +2,13 @@ class ConclusionsController < ApplicationController
   include PartyAuthentication
 
   def show
+    remove_current_party_from_session
+  end
+
+  private
+
+  def remove_current_party_from_session
+    session.delete(:current_party_id)
+    Current.party = nil
   end
 end
