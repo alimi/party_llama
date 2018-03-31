869a62fbef219c553bc0499561c9f24b3a4478b0
Ali Ibrahim <aibrahim2k2@gmail.com>
2018-03-31 15:30:59 -0400

    Add high level tests around voice workflow

---
+++ test/integration/voice_party_responses_test.rb
@@ -0,0 +1,86 @@
+require "test_helper"
+
+class VoicePartyResponsesTest < ActionDispatch::IntegrationTest
+  setup do
+    @party = Party.create!(
+      name: "The Avengers",
+      reservation_code: "232425",
+      responses_end_at: 1.week.from_now
+    )
+  end
+
+  test "a party submits their responses" do
+    @party.guests.create!(first_name: "Black", last_name: "Panther")
+    @party.guests.create!(first_name: "Iron", last_name: "Man")
+    @party.guests.create!(first_name: "Captain", last_name: "America")
+
+    get new_voice_session_path
+
+    assert_includes(
+      xml_response.Response.Gather.Say.content,
+      "enter your reservation code"
+    )
+
+    post_to_next_path params: { "Digits" => @party.reservation_code }
+
+    assert_includes(
+      xml_response.Response.Gather.Say.content,
+      "Are we speaking with The Avengers?"
+    )
+
+    post_to_next_path params: { "SpeechResult" => "Yes.", "Confidence" => ".9" }
+
+    assert_match(
+      /everyone.*attending.*Patterson Park/,
+      xml_response.Response.Gather.Say.content
+    )
+
+    post_to_next_path params: { "SpeechResult" => "No.", "Confidence" => ".9" }
+
+    assert_match(
+      /Will Black be attending.*Patterson Park/,
+      xml_response.Response.Gather.Say.content
+    )
+
+    post_to_next_path params: { "SpeechResult" => "Yes.", "Confidence" => ".9" }
+
+    assert_match(
+      /Will Iron be attending.*Patterson Park/,
+      xml_response.Response.Gather.Say.content
+    )
+
+    post_to_next_path params: { "SpeechResult" => "Yes.", "Confidence" => ".9" }
+
+    assert_match(
+      /Will Captain be attending.*Patterson Park/,
+      xml_response.Response.Gather.Say.content
+    )
+
+    post_to_next_path params: { "SpeechResult" => "No.", "Confidence" => ".9" }
+
+    assert_match(
+      /everyone.*attending.*Douglass-Myers/,
+      xml_response.Response.Gather.Say.content
+    )
+
+    post_to_next_path(
+      params: { "SpeechResult" => "Yes.", "Confidence" => ".9" },
+      follow_redirect: false
+    )
+
+    assert_match(
+      /Yay!/,
+      xml_response.Response.Say.content
+    )
+  end
+
+  def post_to_next_path(options = {})
+    follow_redirect = options.delete(:follow_redirect) { true }
+    post xml_response.Response.Gather["action"], options
+    follow_redirect! if follow_redirect
+  end
+
+  def xml_response
+    Nokogiri::Slop(response.parsed_body)
+  end
+end
