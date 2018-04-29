41b68565f4649f6472a7ef19de1da87c4915eb58
Ali Ibrahim <aibrahim2k2@gmail.com>
2018-04-29 17:06:49 -0400

    Add info to admin party index

--- app/controllers/admin/parties_controller.rb
+++ app/controllers/admin/parties_controller.rb
@@ -2,5 +2,20 @@ class Admin::PartiesController < ApplicationController
   include AdminAuthentication
 
   def index
+    @parties = Party.includes(:guests).all
+
+    @invited_guest_count = Guest.count
+
+    @attending_patterson_park_count = Guest.joins(:party).
+      where.not(parties: { responses_submitted_at: nil }).
+      where(attending_patterson_park: true).
+      count
+
+    @attending_douglass_myers_count = Guest.joins(:party).
+      where.not(parties: { responses_submitted_at: nil }).
+      where(attending_douglass_myers: true).
+      count
+
+    @no_response_party_count = Party.where(responses_submitted_at: nil).count
   end
 end
