5bdc04daed29155344f21c038b2ba029d7523857
Ali Ibrahim <aibrahim2k2@gmail.com>
2018-06-09 15:43:51 -0400

    Update party responses index
      * Add more realistic copy and make the code less rigid

--- app/controllers/party_responses_controller.rb
+++ app/controllers/party_responses_controller.rb
@@ -3,46 +3,14 @@ class PartyResponsesController < ApplicationController
   include ResponseEligibilityFilter
 
   def index
-    @party_responses = PartyResponses.new(Current.party)
+    @patterson_park_guests = Guests.new(
+      *Current.party.guests.partition(&:attending_patterson_park)
+    )
+
+    @douglass_myers_guests = Guests.new(
+      *Current.party.guests.partition(&:attending_douglass_myers)
+    )
   end
 
-  PartyResponses = Struct.new(:party) do
-    def for_patterson_park
-      GuestResponses.new(patterson_park_guests).to_s
-    end
-
-    def for_douglass_myers
-      GuestResponses.new(douglass_myers_guests).to_s
-    end
-
-    private
-
-    def patterson_park_guests
-      @patterson_park_guests ||=
-        party.guests.where(attending_patterson_park: true)
-    end
-
-    def douglass_myers_guests
-      @douglass_myers_guests ||=
-        party.guests.where(attending_douglass_myers: true)
-    end
-  end
-
-  GuestResponses = Struct.new(:guests) do
-    def count
-      guests.count
-    end
-
-    def names
-      guests.map(&:name).to_sentence
-    end
-
-    def to_s
-      if count > 0
-        "#{count} guests attending: #{names}"
-      else
-        "0 guests attending"
-      end
-    end
-  end
+  Guests = Struct.new(:attending, :not_attending)
 end
